044aa762dfed064f41d0827f0b86f08d7d84b233
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-07-04 17:32:39 +0530

    Fixed Dynamic State Emotions

--- client/src/components/StateInfo.js
+++ client/src/components/StateInfo.js
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Grid, Paper, Typography } from "@material-ui/core";
 import DataCharts from "./DataCharts";
 import Tweets from "./Tweets";
 import State from "./State";
+import { Radar } from "react-chartjs-2";
+import { createApolloFetch } from "apollo-fetch";
 
 const StateInfo = (props) => {
+  const [sadnessList, setSadnessList] = useState([]);
+  const [joyList, setJoyList] = useState([]);
+  const [fearList, setFearList] = useState([]);
+  const [disgustList, setDisgustList] = useState([]);
+  const [angerList, setAngerList] = useState([]);
+  const [dataListDefault, setDataListDefault] = useState([30, 30, 30, 30, 30]);
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    const fetch = createApolloFetch({
+      uri: "http://localhost:5000/graphql",
+    });
+
+    fetch({
+      query: `{ sentiments(state: "${props.match.params.name}") { tweet link state city sadness joy fear disgust anger }}`,
+    })
+      .then((res) => {
+        res.data.sentiments.map((sentiment) => {
+          sadnessList.push(sentiment.sadness);
+          joyList.push(sentiment.joy);
+          fearList.push(sentiment.fear);
+          disgustList.push(sentiment.disgust);
+          angerList.push(sentiment.anger);
+        });
+      })
+      .then(() => {
+        let tempDataList = [];
+        let sum = sadnessList.reduce((a, b) => a + b, 0);
+        let avg = sum / sadnessList.length || 0;
+        // setDataList(...dataList, avg);
+        tempDataList.push(avg.toFixed(2) * 100);
+        sum = joyList.reduce((a, b) => a + b, 0);
+        avg = sum / joyList.length || 0;
+        // setDataList(...dataList, avg);
+        tempDataList.push(avg.toFixed(2) * 100);
+        sum = fearList.reduce((a, b) => a + b, 0);
+        avg = sum / fearList.length || 0;
+        // setDataList(...dataList, avg);
+        tempDataList.push(avg.toFixed(2) * 100);
+        sum = disgustList.reduce((a, b) => a + b, 0);
+        avg = sum / disgustList.length || 0;
+        // setDataList(...dataList, avg);
+        tempDataList.push(avg.toFixed(2) * 100);
+        sum = angerList.reduce((a, b) => a + b, 0);
+        avg = sum / angerList.length || 0;
+        // setDataList(...dataList, avg);
+        tempDataList.push(avg.toFixed(2) * 100);
+
+        setDataList(tempDataList);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(dataList);
+  }, [dataList]);
+
   console.log(props.match.params.name);
+
+  const dataToChart = {
+    labels: ["sadness", "joy", "fear", "digust", "anger"],
+    datasets: [
+      {
+        label: props.match.params.name,
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        pointBackgroundColor: "rgba(179,181,198,1)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(179,181,198,1)",
+        data: dataList,
+      },
+    ],
+  };
 
   return (
     <div>
@@ -16,7 +90,11 @@ const StateInfo = (props) => {
           </Paper>
           <Paper style={{ marginTop: "10px", padding: "10px" }}>
             <Typography>Charts Here</Typography>
-            <DataCharts type="radar" />
+            {true ? (
+              <Radar data={dataToChart} redraw key={Math.random()} />
+            ) : (
+              <p>Loading...</p>
+            )}
           </Paper>
           <Paper style={{ marginTop: "10px", padding: "10px" }}>
             <Tweets selectedState={props.match.params.name} />
