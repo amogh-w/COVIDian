22999b03419fcc109b699e465da4688e27655142
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-07-05 14:49:37 +0530

    Added Resources Content

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -1,9 +1,272 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./ResourcesCards.css";
+import { Line, Bar } from "react-chartjs-2";
+import styles2 from "./ResourcesCharts.css";
+import CountUp from "react-countup";
+import cx from "classnames";
+
+const url = "https://covid19.mathdro.id/api";
+
+const fetchData = async (country) => {
+  let changeableUrl = url;
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
+
+    return {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+    return modifiedData;
+  } catch (error) {}
+};
+
+const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Cards = ({
+  data: { confirmed, recovered, deaths, lastUpdate },
+  country,
+}) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+  const active = confirmed["value"] - recovered["value"] - deaths["value"];
+  let cardDetails = [
+    {
+      style: styles.infected,
+      text: "Infected",
+      value: confirmed.value,
+      bottomText: "Number of infect cases of COVID-19",
+    },
+    {
+      style: styles.recovered,
+      text: "Recovered",
+      value: recovered.value,
+      bottomText: "Number of recoveries from COVID-19",
+    },
+    {
+      style: styles.deaths,
+      text: "Deaths",
+      value: deaths.value,
+      bottomText: "Number of deaths caused by COVID-19",
+    },
+    {
+      style: styles.active,
+      text: "Active",
+      value: active,
+      bottomText: "Number of active cases of COVID-19",
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <Grid
+        container
+        spacing={3}
+        justify="center"
+        style={{ marginBottom: "50px" }}
+      >
+        {cardDetails.map((detail, index) => (
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={2}
+            className={cx(styles.Card, detail.style)}
+            key={index}
+            style={{ margin: "0px 23.675px", padding: "12px" }}
+          >
+            <CardContent>
+              <Typography color="textPrimary" gutterBottom>
+                <b>{detail.text}</b>
+              </Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={detail.value}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              <Typography color="textPrimary">Last Updated at : </Typography>
+              <Typography color="textSecondary" variant="body2">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+              <Typography color="textSecondary" variant="body2">
+                {new Date(lastUpdate).toLocaleTimeString()}
+              </Typography>
+              <Typography variant="body2">{detail.bottomText}</Typography>
+              <Typography color="textPrimary"> {country} </Typography>
+            </CardContent>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchAPI();
+  }, []);
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ["Infected", "Recovered", "Deaths", "Active"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
+              "rgba(242, 234, 0, 0.5)",
+            ],
+            hoverBackgroundColor: [
+              "rgba(0, 77, 153)",
+              "rgba(30, 102, 49)",
+              "rgba(255, 51, 51)",
+              "rgba(204, 153, 0)",
+            ],
+            data: [
+              confirmed.value,
+              recovered.value,
+              deaths.value,
+              confirmed.value - (recovered.value + deaths.value),
+            ],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${country}` },
+      }}
+    />
+  ) : null;
+
+  return (
+    <div className={styles2.container}>{country ? barChart : lineChart}</div>
+  );
+};
+
+const CountryPicker = ({ handleCountryChange }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+    fetchAPI();
+  }, [setFetchedCountries]);
+
+  return (
+    <FormControl className={styles2.formControl}>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="">Global</option>
+        {fetchedCountries.map((country, key) => (
+          <option key={key} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
 
 const Resources = () => {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState("");
+
+  useEffect(() => {
+    const mango = async () => {
+      const fetchedData = await fetchData();
+      setData(fetchedData);
+    };
+    mango();
+  }, []);
+
+  const handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    setData(fetchedData);
+    setCountry(country);
+  };
+
   return (
-    <div>
-      <p>Resources</p>
+    <div className={styles.container}>
+      {/* <img className={styles.image} src={coronaImage} alt="COVID-19" /> */}
+      <br />
+      <text>
+        <Typography>Global and Country Wise Cases of Corona Virus</Typography>
+      </text>
+      <br />
+      <Typography>
+        <i>(For a particular select a Country from below)</i>
+      </Typography>
+      <br />
+      <br />
+      <Cards data={data} country={country} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
     </div>
   );
 };
