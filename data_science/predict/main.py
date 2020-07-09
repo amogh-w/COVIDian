3d5cce0ef8d4f783cdbbb54d2c89754f5376bafd
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-07-10 01:40:13 +0530

    Get Labels from POST

--- data_science/predict/main.py
+++ data_science/predict/main.py
@@ -5,6 +5,7 @@ import os
 from starlette.middleware.cors import CORSMiddleware
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
 # import uvicorn
 
@@ -17,22 +18,25 @@ app.add_middleware(
 # print(os.listdir("/app"))
 
 # model = pickle.load(open("/app/models/lstm/model_amogh.pickle", "rb",))
-# model = load_model(
-#     "/Users/amogh/Documents/Study/twitter/data_science/predict/models/lstm/model.h5"
-# )
-model = load_model("/app/models/lstm/model.h5")
+model = load_model(
+    "/Users/amogh/Documents/Study/twitter/data_science/predict/models/lstm/model.h5"
+)
+# model = load_model("/app/models/lstm/model.h5")
 # tokenizer = pickle.load(open("/app/models/lstm/tokenizer.pickle", "rb"))
-# tokenizer = pickle.load(
-#     open(
-#         "/Users/amogh/Documents/Study/twitter/data_science/predict/models/lstm/tokenizer.pickle",
-#         "rb",
-#     )
-# )
-tokenizer = pickle.load(open("/app/models/lstm/tokenizer.pickle", "rb",))
+tokenizer = pickle.load(
+    open(
+        "/Users/amogh/Documents/Study/twitter/data_science/predict/models/lstm/tokenizer.pickle",
+        "rb",
+    )
+)
+# tokenizer = pickle.load(open("/app/models/lstm/tokenizer.pickle", "rb",))
 
 
 class Data(BaseModel):
     tweet: str
+
+
+score_list = ["anger", "happiness", "neutral", "sadness", "worry"]
 
 
 @app.post("/predict")
@@ -43,5 +47,4 @@ def predict(data: Data):
     sequences = tokenizer.texts_to_sequences(input_string)
     X_processed = pad_sequences(sequences, padding="post", maxlen=48)
     score = model.predict(X_processed)
-
-    return {"prediction": str(score)}
+    return {"prediction": score_list[np.argmax(score)]}
