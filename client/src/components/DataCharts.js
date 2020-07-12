e3c194ae93a15e1e4e89afe182a155e656a8d3d0
rampa2510 <iamram2510@gmail.com>
2020-07-12 22:31:27 +0530

    Removed redundant code

--- client/src/components/DataCharts.js
+++ client/src/components/DataCharts.js
@@ -5,61 +5,6 @@ import useTheme from "@material-ui/core/styles/useTheme";
 import { createApolloFetch } from "apollo-fetch";
 
 
-const data2 = {
-  labels: ["sadness", "joy", "fear", "digust", "anger"],
-  datasets: [
-    {
-      label: "Mumbai",
-      backgroundColor: "rgba(179,181,198,0.2)",
-      borderColor: "rgba(179,181,198,1)",
-      pointBackgroundColor: "rgba(179,181,198,1)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgba(179,181,198,1)",
-      data: [23, 28, 7, 18, 16],
-    },
-    {
-      label: "Delhi",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgba(255,99,132,1)",
-      pointBackgroundColor: "rgba(255,99,132,1)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgba(255,99,132,1)",
-      data: [26, 28, 9, 19, 15],
-    },
-    {
-      label: "Hyderabad",
-      backgroundColor: "rgba(50, 210, 61,0.2)",
-      borderColor: "rgba(50, 210, 61,1)",
-      pointBackgroundColor: "rgba(50, 210, 61,1)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgba(50, 210, 61,1)",
-      data: [18, 25, 7, 26, 16],
-    },
-    {
-      label: "Chennai",
-      backgroundColor: "rgba(5, 143, 255,0.2)",
-      borderColor: "rgba(5, 143, 255,1)",
-      pointBackgroundColor: "rgba(5, 143, 255,1)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgba(5, 143, 255,1)",
-      data: [29, 21, 10, 15, 16],
-    },
-    {
-      label: "Kolkata",
-      backgroundColor: "rgba(255, 5, 247,0.2)",
-      borderColor: "rgba(255, 5, 247,1)",
-      pointBackgroundColor: "rgba(255, 5, 247,1)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgba(255, 5, 247,1)",
-      data: [22, 24, 10, 20, 16],
-    },
-  ],
-};
 
 const DataCharts = ({ type }) => {
   const [keyArr,setKeyArr] = React.useState([])
