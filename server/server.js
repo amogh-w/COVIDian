6de94e103618555c7c6c71e428753954a6c37898
rampa2510 <iamram2510@gmail.com>
2020-07-14 22:16:45 +0530

    Complted backend code for backend of chatbot

--- server/server.js
+++ server/server.js
@@ -5,11 +5,61 @@ const graphqlHTTP = require("express-graphql");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-
+const chatBot = require('./chatbot')
 const schema = require("./schema/schema");
+const {createApolloFetch} = require('apollo-fetch')
+const Nfetch = require('node-fetch');
 
 app.use(cors());
+app.use(express.json())
 
+const fetch = createApolloFetch({
+  uri: `http://localhost:4000/graphql`,
+});
+
+
+app.post('/chat',async (req,res)=>{
+  // console.log(req.body.msg)
+  const result = await chatBot(req.body.msg)
+  // console.log(result)
+  if(result.intent.displayName==='Default Fallback Intent') return res.send({msg:"Invalid query"})
+
+  const statesArr = result.parameters.fields.state.listValue.values
+  const need = result.parameters.fields.need.listValue.values
+  const casesData = []
+  if(need[0]==='cases'){
+    const [,countryData] = await Nfetch('https://covid-19india-api.herokuapp.com/all').then(data=>data.json())
+    statesArr.forEach(data=>{
+      const requiredData = countryData.state_data.find(stateData=>stateData.state.toLowerCase()===data.stringValue.toLowerCase())
+      if(requiredData) casesData.push({name:data.stringValue,...requiredData})
+      else casesData.push({name:data.stringValue})
+    })
+    // const requiredData = countryData.state_data.find(stateData=>stateData.state.toLowerCase()===state.toLowerCase())
+    return res.send({data:casesData})
+  }
+
+  let query = ""
+  // if (need==='emotion')
+
+  // const emotionData = await fetch({
+  //   query: `{
+  //     sentiments(state: "${state}") {
+  //     id
+  //     date_time
+  //     tweet
+  //     link
+  //     state
+  //     city
+  //     anger
+  //     happiness
+  //     neutral
+  //     sadness
+  //     worry
+  //     }
+  //   }`,
+  // }).then(data=>console.log())
+  res.end()
+})
 
 app.listen(process.env.covidian_server_internal_port || 4000, () => {
   console.log(
@@ -25,7 +75,7 @@ app.use(
   })
 );
 
-mongoose.connect(`${process.env.covidian_server_internal_mongo_url}`, {
+mongoose.connect(`mongodb+srv://amogh123:amogh123@amogh-first-cluster-vdru3.mongodb.net/covidian`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
